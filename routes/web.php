1cbec53a32eb33ed719ba18e0fe3afd456426fd6
nishimura <reitonishimura1227@gmail.com>
2025-08-05 09:16:05 +0000

    mockup

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
+Route::get('/welcome/second', [WelcomeController::class, 'second']);
