12849c9ea705474bcb164b535485028000d1b3af
nishimura <reitonishimura1227@gmail.com>
2025-08-09 15:03:33 +0000

    ページネーションを書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -45,11 +45,18 @@
         @endforeach
         </table>
         <!-- ページネーション -->
-        現在 1 ページ目<br>
-        <a href="./top.html">最初のページ(未実装)</a> / 
-        <a href="./top.html">前に戻る(未実装)</a> / 
-        <a href="./top.html">次に進む(未実装)</a>
-        <br>
+        {{ --$list->links()-- }}
+        現在 {{ $list->currentPage() }} ページ目<br>        <a href="./top.html">最初のページ(未実装)</a> / 
+        @if ($list->onFirstPage() === false)
+        <a href="/task/list">最初のページ</a>
+        @else
+        最初のページ
+        @endif
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        @else
+            次に進む
+        @endif        <br>
         <hr>
         <menu label="リンク">
         <a href="/logout">ログアウト</a><br>
