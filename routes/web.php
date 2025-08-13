33cb8a6bba3480575883bebfd887ad5091dfd8eb
nishimura <reitonishimura1227@gmail.com>
2025-08-13 11:20:20 +0000

    CSVダウンロード機能を書いた

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/edit/{task_id}', [TaskController::class, 'edit'])->whereNumber('task_id')->name('edit');
         Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');
         Route::get('/csv/download', [TaskController::class, 'csvDownload']);
+     Route::post('/complete/{task_id}', [TaskController::class, 'complete'])->whereNumber('task_id')->name('complete');  
     });
 
     Route::get('/logout', [AuthController::class, 'logout']);
