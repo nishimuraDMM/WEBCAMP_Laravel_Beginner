45e96fa300ddcee99004a49181685ad3b5a28166
nishimura <reitonishimura1227@gmail.com>
2025-08-16 05:23:53 +0000

    6

--- resources/views/task/detail.blade.php
+++ resources/views/task/detail.blade.php
@@ -5,21 +5,18 @@
 
 {{-- メインコンテンツ --}}
 @section('contents')
-<h1>タスク詳細閲覧</h1>
+        <h1>タスク詳細閲覧</h1>
+        @if (session('front.task_edit_success') == true)
+            タスクを編集しました！！<br>
+        @endif
+
         タスク名： {{ $task->name }}<br>
         期限： {{ $task->period }}<br>
         重要度： {{ $task->getPriorityString() }}<br>
         タスク詳細： <pre>{{ $task->detail }}</pre><br>
         <hr>
-        <hr>
-        <form action="{{ route('delete', ['task_id' => $task->id]) }}" method="post">
-            @csrf
-            @method("DELETE")
-            <button onclick='return confirm("このタスクを削除します(削除したら戻せません)。よろしいですか？");'>タスクを削除する</button>
-        </form>        
         <menu label="リンク">
         <a href="/task/list">タスク一覧</a><br>
         <a href="/logout">ログアウト</a><br>
         </menu>
-        
-@endsection         
+@endsection
